c7c6e27528c39537941d5d3d2b5f55095cb0ac00
almero.steyn <almero.steyn@gmail.com>
2017-08-04 14:15:02 +0200

    Content

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+if (process.env.NODE_ENV !== 'production') {
+  const axe = require('react-axe');
+  axe(React, ReactDOM, 1000);
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
